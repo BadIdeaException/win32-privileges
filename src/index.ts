f566364015212b803e897d0bdeedf86d82c153d0
Christian Heger <7235795+BadIdeaException@users.noreply.github.com>
2026-04-13 17:44:49 +0200

    docs: improve docs for API functions and Privilege class

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,24 @@ import * as advapi32 from './advapi32.js';
 import Privilege, { type PrivilegeName } from './privilege.js';
 
 /**
- * Queries the kernel for the privileges assigned to the current process. 
- * @returns An array of `Privilege` objects
- * @throws When called from an OS other than Windows 
+ * Retrieves all privileges associated with the current process token, 
+ * regardless of whether they are enabled or not. A privilege that is present
+ * but disabled is potentially available to this process, i.e. within the scope
+ * of permitted operations for the user owning the process. Only enabled privileges,
+ * however, actually entitle the process to carry out the associated operations.
+ * 
+ * For example, a process may have the `SE_SHUTDOWN_PRIVILEGE` privilege present. This means
+ * that it can potentially shut down the system, but it cannot do so currently (because the
+ * privilege is disabled).
+ * To actually shut down the system, the process requires an enabled `SE_SHUTDOWN_PRIVILEGE`. 
+ * If the privilege is not present at all, the process can never shut down the system.
+ * 
+ * Use {@link getEnabledPrivileges} if you want to get a list of enabled privileges.
+ * 
+ * @returns {Privilege[]} An array of {@link Privilege} objects representing
+ * all privileges assigned to the current process.
+ * 
+ * @throws {Error} If called on platform other than Windows.
  */
 export function getPrivileges(): Privilege[] {
 	if (process?.platform !== 'win32') 
@@ -22,14 +37,48 @@ export function getPrivileges(): Privilege[] {
 	}
 }
 
+/**
+ * Checks whether the current process has a specific privilege assigned,
+ * regardless of whether the privilege is enabled or not.
+ *
+ * See {@link getPrivileges} for details on present vs enabled privileges.
+ * 
+ * Use {@link hasEnabledPrivilege} instead if you want to check for enabled privileges.
+ *
+ * @param {PrivilegeName} privilege - The name of the privilege to check.
+ * @returns {boolean} `true` if the privilege exists in the process token;
+ * otherwise `false`.
+ * @throws {Error} If called on platform other than Windows.
+ */
 export function hasPrivilege(privilege: PrivilegeName): boolean {
 	return getPrivileges().some(priv => priv.name === privilege);
 }
 
+/**
+ * Retrieves all enabled privileges associated with the current process token.
+ * These privileges are active, i.e. entitle the process to carry out the associated
+ * operations.
+ *
+ * See {@link getPrivileges} for details on present vs enabled privileges.
+ * 
+ * @returns {Privilege[]} An array of enabled {@link Privilege} objects.
+ * @throws {Error} If called on platform other than Windows.
+ */
 export function getEnabledPrivileges(): Privilege[] {
 	return getPrivileges().filter(priv => priv.isEnabled());
 }
 
+/**
+ * Checks whether a specific privilege is present and enabled for the
+ * current process.
+ *
+ * See {@link getPrivileges} for details on present vs enabled privileges.
+ *
+ * @param {PrivilegeName} privilege - The name of the privilege to check.
+ * @returns {boolean} `true` if the privilege exists and is enabled;
+ * otherwise `false`.
+ * @throws {Error} If called on platform other than Windows.
+ */
 export function hasEnabledPrivilege(privilege: PrivilegeName): boolean {
 	return getEnabledPrivileges().some(priv => priv.name === privilege);
 }
--- src/privilege.ts
+++ src/privilege.ts
@@ -1,8 +1,18 @@
 import { SE_PRIVILEGE_ENABLED_BY_DEFAULT, SE_PRIVILEGE_ENABLED, SE_PRIVILEGE_USED_FOR_ACCESS } from './advapi32.js'
 import { SE_CREATE_TOKEN_NAME, SE_ASSIGNPRIMARYTOKEN_NAME, SE_LOCK_MEMORY_NAME, SE_INCREASE_QUOTA_NAME, SE_UNSOLICITED_INPUT_NAME, SE_MACHINE_ACCOUNT_NAME, SE_TCB_NAME, SE_SECURITY_NAME, SE_TAKE_OWNERSHIP_NAME, SE_LOAD_DRIVER_NAME, SE_SYSTEM_PROFILE_NAME, SE_SYSTEMTIME_NAME, SE_PROF_SINGLE_PROCESS_NAME, SE_INC_BASE_PRIORITY_NAME, SE_CREATE_PAGEFILE_NAME, SE_CREATE_PERMANENT_NAME, SE_BACKUP_NAME, SE_RESTORE_NAME, SE_SHUTDOWN_NAME, SE_DEBUG_NAME, SE_AUDIT_NAME, SE_SYSTEM_ENVIRONMENT_NAME, SE_CHANGE_NOTIFY_NAME, SE_REMOTE_SHUTDOWN_NAME, SE_UNDOCK_NAME, SE_SYNC_AGENT_NAME, SE_ENABLE_DELEGATION_NAME, SE_MANAGE_VOLUME_NAME, SE_IMPERSONATE_NAME, SE_CREATE_GLOBAL_NAME, SE_TRUSTED_CREDMAN_ACCESS_NAME, SE_RELABEL_NAME, SE_INC_WORKING_SET_NAME, SE_TIME_ZONE_NAME, SE_CREATE_SYMBOLIC_LINK_NAME, SE_DELEGATE_SESSION_USER_IMPERSONATE_NAME } from './advapi32.js';
 import type { PrivilegeName } from './advapi32.js';
+export { type PrivilegeName } from './advapi32.js';
 
-/** Represents a single privilege. */
+/** 
+ * Represents a single privilege. 
+ *
+ * A privilege is the right of a user to perform various system-related operations, such as shutting down the system, loading device drivers, or changing the system time. 
+ * A user's access token contains a list of the privileges held by either the user or the user's groups. These privileges are inherited by processes owned by the user.
+ *
+ * A `Privilege` consists of the privilege's name, and its activation status.
+ *
+ * This class exposes all [privilege names](https://learn.microsoft.com/en-us/windows/win32/secauthz/privilege-constants) as static properties (e.g. `Privilege.SE_CREATE_TOKEN_NAME`).
+ */
 export default class Privilege {
 	name: string;
 	status: number;
@@ -44,27 +54,33 @@ export default class Privilege {
 	static SE_CREATE_SYMBOLIC_LINK_NAME: PrivilegeName = SE_CREATE_SYMBOLIC_LINK_NAME;
 	static SE_DELEGATE_SESSION_USER_IMPERSONATE_NAME: PrivilegeName = SE_DELEGATE_SESSION_USER_IMPERSONATE_NAME;
 
-	constructor(name: string, status: number) {
+	constructor(name: PrivilegeName, status: number) {
 		this.name = name;
 		this.status = status;
 	}
 
 	/**
-	 * Whether this privilege is enabled.
+	 * Whether this privilege is currently enabled.
+	 *
+	 * Only enabled privileges entitle the process to carry out the associated operations.
 	 */
 	isEnabled(): boolean {
 		return Boolean(this.status & SE_PRIVILEGE_ENABLED);
 	}
 
 	/**
-	 * Whether this privilege is normally enabled by default.
+	 * Whether this privilege is enabled by default when the token is created.
+	 *
+	 * A privilege that is enabled by default may still be disabled later.
 	 */
 	isEnabledByDefault(): boolean {
 		return Boolean(this.status & SE_PRIVILEGE_ENABLED_BY_DEFAULT);
 	}
 
 	/**
-	 * Whether this privilege has been used to gain access to an object.
+	 * Whether this privilege has actually been used. 
+	 * (Specifically, whether it has contributed to at least one
+	 * successful access check.)
 	 */
 	wasUsedForAccess(): boolean {
 		return Boolean(this.status & SE_PRIVILEGE_USED_FOR_ACCESS);
